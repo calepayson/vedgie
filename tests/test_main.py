736b4d99f0f1141389f0840ecf85db167609ac49
Cale Smith <calepayson@mac.com>
2024-06-20 09:56:10 -0700

    Add test

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_read_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, World"}
